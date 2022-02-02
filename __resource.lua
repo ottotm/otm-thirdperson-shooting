b6ae3b5fae0c4b521cc306a8849e983d388a69a2
ottotm <75931482+ottotm@users.noreply.github.com>
2022-02-02 23:37:17 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,14 @@
+description 'Thirdperson shooting with crosshair. Made by ottom, discord: #ottotm'
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ui_page 'html/index.html'
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/listener.js'
+}
